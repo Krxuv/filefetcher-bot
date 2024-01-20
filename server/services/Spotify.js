7e7f8fa10417146de581619f3da536cd15a84553
Krxuv <141473626+Krxuv@users.noreply.github.com>
2024-01-21 05:43:21 +0800

    update Spotify.js

--- server/services/Spotify.js
+++ server/services/Spotify.js
@@ -24,6 +24,7 @@ class Spotify {
     try {
       let { data } = await axios.get(`https://api.spotifydown.com/${endpoint}/${id}`, {
         headers: {
+          'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
           'Origin': 'https://spotifydown.com',
           'Referer': 'https://spotifydown.com/',
         }
